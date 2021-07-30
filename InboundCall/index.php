884344e0fb658f92f1bee08fce4189adcdda3476
root <root@ip-172-31-29-77.ec2.internal>
2021-07-30 01:58:29 +0000

    QOL Update

--- InboundCall/index.php
+++ InboundCall/index.php
@@ -3,17 +3,18 @@ echo header('content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 /** Get sip endpoint list **/
 $params=$_REQUEST['SipUser'];
+//$from=$_REQUEST['Caller'];
 $toNumberList=explode(",",$params);
+// If multiple numbers/SIPs are used, will call sequentially, regardless
+// of answering
 ?>
 <Response>
     <?php
     for($i=0; $i < sizeof($toNumberList) ;$i++) { $to=$toNumberList[$i]; ?>
-        <Dial timeout="20">
-
+        <Dial timeout="20" ><!-- callerId="<?php echo $from; ?>"> -->
             <Sip>
                 <?php echo $to; ?>
             </Sip>
-
         </Dial>
         <?php } ?>
 </Response>
